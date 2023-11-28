9e9a1d01933014246652738e22d67a245a8c80c8
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2023-11-27 17:17:16 -0800

    Later to be completed

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * check_cycle - This function that check for cyclye will be implemented later
+ * @list: pointer to the list
+ * Return: 0 if no cycle and 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,8 @@
+#ifndef __LISTS_H__
+#define __LISTS_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int check_cycle(listint_t *list);
+#endif
