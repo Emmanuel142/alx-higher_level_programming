b4e8cfd41f6585870d3056c6cdf8993e03c3445c
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2023-11-28 12:53:33 -0800

    Abs is gotten through hard exercise

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last = number % 10
+last = abs(number) % 10
 if last == 0:
     print(f"Last digit of {number:d} is {last:d} and is 0")
 elif last < 6:
