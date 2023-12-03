34a338a78c236bad4b12658ce72bb0cb40aa261a
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2023-12-03 11:18:17 -0800

    pint

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,3 +3,9 @@ if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         for item in my_list:
             print('{}'.format(item))
+
+    def hello():
+        hi = [2, 3  , 3, 6,3]
+        print_list_integer(hi)
+
+    hello()
