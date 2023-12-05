ae39c255d22684ac671da7aec12722303eb34d9a
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2023-12-04 19:48:37 -0800

    Data structure

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for item in my_list:
-            print('{}'.format(item))
-
-    def hello():
-        hi = [2, 3  , 3, 6,3]
-        print_list_integer(hi)
-
-    hello()
+def print_list_integer(my_list=[]):
+    for item in my_list:
+        print('{:d}'.format(item))
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    print("hello")
+
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return
+    return my_list[idx]
