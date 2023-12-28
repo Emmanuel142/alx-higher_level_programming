1ba344d908ae5cc852c845d6187553b0bdb08133
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2023-12-27 23:57:49 -0800

    google string added

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
+"""Square modules
+This modules create an empty square class
+Todo:
+    create an empty class
+"""
 def main():
+    """ The beginning of the program
+    Args:
+        No arguments
+    Return:
+        Nothing
+    """
     class Square:
         pass
+
+
+    pass
+
+
 if __name__ == "__main__":
     main()
