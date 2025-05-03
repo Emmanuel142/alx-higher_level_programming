9b567cc42b0f155d0b67a025fc68bb9986b5d41d
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-03 14:15:21 +0100

    Printing square

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 
-Square = __import__('4-square').Square
+Square = __import__('5-square').Square
 
-S = Square(5)
-try:
-    print (S.area())
-    print(S.size)
-
-    S.size = 7
-
-    print(S.area())
-    S.size = "8"
-    print(S.size)
-    print(S.area())
-except Exception as e:
-    print(e)
+my_square = Square(5)
+print(my_square.size)
+print(my_square.area())
+my_square.my_print()
