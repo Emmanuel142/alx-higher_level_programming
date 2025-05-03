00a93d4fa717c115f6b04fb68d385df4765b1e25
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-03 13:41:22 +0100

    value to size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -47,8 +47,8 @@ class Square:
         value (int): The size of the square
         """
         if type(value) is not int:
-            raise TypeError("value must be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("value must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
