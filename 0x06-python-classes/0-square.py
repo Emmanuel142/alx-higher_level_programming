ff06cf0ec90c0a894ce0673dbfd48ed0fbdab039
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-03 00:16:29 -0700

    Empty square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This module create a class of empty Square
+"""
+
+
+class Square:
+    """This class make a square
+
+    Args:
+        No argument
+    Attribute:
+        No attribute
+    """
+
+    pass
