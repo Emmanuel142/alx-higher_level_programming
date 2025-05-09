14cedaf9c3cfb813f5cd23f24b84d2e35feec734
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-09 13:16:16 +0100

    Exception sshh:

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,7 +9,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except Exception as e:
-        print(e)
+        pass
     finally:
         print("Inside result: {}".format(result))
     return result
