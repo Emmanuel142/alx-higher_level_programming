b1d58ec18d4f206c77e460d1de6cba7856338232
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-09 12:48:38 +0100

    exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -17,7 +17,7 @@ def safe_print_list_integers(my_list=[], x=0):
             printed_count += 1
         except (ValueError, TypeError):
             pass
-        except IndexError:
-            break
+        except IndexError as e:
+            print(e)
     print()
     return printed_count
