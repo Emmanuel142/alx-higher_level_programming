ce7aceff39f3654a1979680afd82e05c60165eb4
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-09 11:34:06 +0100

    1-safe_integer

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+safe_print_list = __import__('1-safe_print_integer').safe_print_integer
 first_print = [1,2,3,4,7,6]
 second_print = ["a",5,"b",6,"bard"]
+third_print = 3
 
-safe_print_list(first_print, 3)
-safe_print_list(second_print, 4)
+safe_print_list(3)
+safe_print_list(second_print)
+
 
 
