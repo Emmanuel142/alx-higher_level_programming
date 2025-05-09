642ee4d140a27d845f7cafccbb52d701ee77cb81
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-09 11:58:52 +0100

    2-print_list_integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module print integers in a list
+"""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """This function print list of integer
+        Args:
+            my_list[] (list): List of items(int, str,...)
+            x (int): The depth of item to print
+        """
+    try:
+        item_count = 0
+        printed_count = 0
+        score = ""
+        for item in my_list:
+            if isinstance(item, int):
+                score += item
+                printed_count += 1
+            else:
+                pass
+            if item_count != x:
+                item_count += 1
+            else:
+                raise Exception('x is bigger than the list')
+        print("{:d}".format(int(score)))
+    except Exception(e):
+        print(e)
