fefa05fd62c8ed6047e29e8e1ca097d6355a2ae4
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-09 10:46:22 +0100

    safe_print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This module print the x element of a list
+"""
+
+def safe_print_list(my_list=[], x=0):
+    """This function print item in a list
+        Args:
+            my_list (list)= This is the list of all types(int,str,...)
+            x (int) = The number of item to be printed
+    try:
+        store = ""
+        count = 0
+        for item in my_list:
+            if x != count:
+                store += str(item)
+                count += 1
+        print(store)
+        return count
+    except:
+        Exception(e)
+        print(e)
---
+++ 0x05-python-exceptions/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+first_print = [1,2,3,4,7,6]
+second_print = ["a",5,"b",6,"bard"]
+
+safe_print_list(first_print, 3)
+safe_print_list(second_print, 4)
+
+
