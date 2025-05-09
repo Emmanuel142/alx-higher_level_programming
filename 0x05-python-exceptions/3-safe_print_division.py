90e87f91165cee0a474216a8333a6647459ec635
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-09 13:05:43 +0100

    division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This module divide two integer
+"""
+
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except Exception as e:
+        print(e)
+    finally:
+        print("Inside result{:d}".format(result))
+    return result
+
