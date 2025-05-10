e8c56321c37d22997876a6790375fb5ddf067739
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-10 23:54:00 +0100

    divide list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""This module divides divides a list of element by another
+"""
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    """This function takes list 1 and divide it with
+    list 2
+    Args:
+        my_list_1 (list): Takes any input(int,str,etc)
+        my_list_2 (list): Takes any input(int,str,etc)
+        list_lenght (int): The total length to be accessed
+    Raises:
+        IndexError: print "out of range"
+        TypeError: print "wrong type"
+        ZeroDivisionError: print "division by 0"
+        """
+    result = None
+    overall = []
+    try:
+        for i in range(list_length):
+            result = my_list_1[i] / my_list_2[i]
+            overall.append(result)
+    except TypeError:
+        print("wrong type")
+    except IndexError:
+        print("out of range")
+    except ZeroDivisionError:
+        print("division by 0")
+    finally:
+        for j in overall:
+            print("{}".format(j))
--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-safe_print_list = __import__('2-safe_print_list_integers').safe_print_list_integers
-first_print = [1,2,3,4,7,6]
-second_print = ["a",5,"b",6,"bard"]
+safe_print_list = __import__('4-list_division').list_division
+first_print = [2, 0, 2, 2, 2,2]
+second_print = [6, 8, 10, 12, 14]
 third_print = 3
 
-safe_print_list(3)
-safe_print_list(second_print, 3)
-safe_print_list(first_print, 9)
+safe_print_list(second_print, first_print, 8)
 
 
 
