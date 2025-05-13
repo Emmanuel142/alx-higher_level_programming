c7f76a991c8eb65f0d84dd8a92f1998c13601cd3
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-13 17:05:13 +0100

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module define a function that print
+    square with #
+    """
+def print_square(size):
+    """This function print square with a #
+    Args:
+        size (int): size of the square
+    Raise:
+        TypeError: if size is not int
+        """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
