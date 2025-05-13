3714e8d29294322817786cd9c4ca49c4b6b60bc0
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-13 14:41:07 +0100

    2-matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 
-"""Okay
+"""This module define a matrix of integers and divide them
 """
+
+
+def matrix_divided(matrix, div):
+        if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not all(all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not isinstance(div, (int, float)):
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+
+        row_length = len(matrix[0])
+        for row in matrix:
+            if len(row) != row_length:
+                raise TypeError("Each row of the matrix must have the same size")
+
+        new_matrix = [[(round(i - div, 2)) for i in row] for row in matrix]
+        return new_matrix
