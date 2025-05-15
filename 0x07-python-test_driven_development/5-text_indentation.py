917f0dc1fdbc92c7f6442c4e0ed2c4b61e037f40
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-15 12:30:25 +0100

    remove newline

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,7 +5,7 @@
 
 def text_indentation(string):
     """This function indent by printing a newline
-    when it enconter symbols like ", / . \"
+    when it enconter symbols like ", ? . :"
     Args:
         string (str): string of word
     """
@@ -22,4 +22,4 @@ def text_indentation(string):
             i += 1
             continue
         i += 1
-    print(new_string)
+    print(new_string, end="")
