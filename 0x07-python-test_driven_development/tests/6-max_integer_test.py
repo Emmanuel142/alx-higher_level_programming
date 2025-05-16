e0d6f3d29d47c4c618df9ca41ffce933ab195013
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-16 10:17:01 +0100

    sythax error fixed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,24 +2,24 @@
 """This is the unittest to determine max integer
 """
 import unittest
-max_integer = __import__('6-max_integer_0').max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaxInteger(unittest.TesCase):
+class TestMaxInteger(unittest.TestCase):
     """This class test for max integer
     """
     def testPositive(self):
-        self.assertEqual(maxinteger([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def testNegative(self):
-        self.assertEqual(maxinteger([-1, -4, -5, -7]), -1)
+        self.assertEqual(max_integer([-1, -4, -5, -7]), -1)
 
     def testMix(self):
-        self.asserEqual(max_integer([3, -3, 5, -7, -4]), 5)
+        self.assertEqual(max_integer([3, -3, 5, -7, -4]), 5)
 
     def testWrongInput(self):
         with self.assertRaises(TypeError):
             max_integer("string")
 
     def testNoneInput(self):
-        self.assertIsNone(max_integer[])
+        self.assertIsNone(max_integer([]))
