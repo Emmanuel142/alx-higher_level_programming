a1fa1a4ee97a637d35f085cde0b82b34c8edce6e
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-18 15:11:57 +0100

    come again later

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module declare a class Mylist that inherit
+    from the class List
+    """
+
+
+class Mylist(list):
+    def __init__(self):
+
+    def print_sorted(self):
+        for i in 
