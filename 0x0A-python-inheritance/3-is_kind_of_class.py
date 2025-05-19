72cf90c8a332fb5c7cac323159e90b360cd87243
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-19 22:29:33 +0100

    is a kind

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module define a function that return True
+    if obj is an instance of a class
+    """
+
+
+def is_kind_of_class(obj, a_class):
+    """This function return true if object is an instance
+    of a class or is a subclass of a class else false
+    """
+    if type(obj) is a_class or issubclass(obj, a_class):
+        return True
+    return False
