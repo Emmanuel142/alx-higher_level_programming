1d2b3be6d5d614394046de912f6578fe0f746019
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-05-19 22:07:47 +0100

    pycodestyled

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,5 +5,9 @@
 
 
 class MyList(list):
+    """This class print sort list
+    """
     def print_sorted(self):
+        """This is the method to print sorted list
+        """
         return print(sorted(self))
