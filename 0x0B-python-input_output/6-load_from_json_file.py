257a0032c1e990662746788d2d9a7bfc9411d231
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-07-16 16:32:59 +0100

    load json from file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,14 +1,12 @@
-#!/bin/usr/python3
-
+#!/usr/bin/python3
 """This module create and object from json file
 """
 import json
 
 
-def loads_from_json_file(filename):
-    """This function loads from json file
-    and save into file 'filename'
+def load_from_json_file(filename):
+    """This function loads from json file 'filename'
+    and return the corresponding Object
     """
     with open(filename, encoding="utf-8") as f:
-        j_file = json.load(filename)
-        f.read(jfile)
+        return json.load(f)
