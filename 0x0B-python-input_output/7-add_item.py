ce8097046d5baf2303ed2a670188a011906b6cbf
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-07-16 17:48:58 +0100

    styled and documented

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""This module take arguments and save it in a
+    json file
+    Module:
+        save_to_json_file (fnc) = takes input and save to
+                                a specified json file
+        load_from_json_file = load a json file for use
 """
-"""
-import sys, os
+import sys
+import os
 
 
 save_to_json = __import__('5-save_to_json_file').save_to_json_file
@@ -16,7 +22,7 @@ else:
 if len(sys.argv) < 2:
     save_to_json(my_list, filename)
 else:
-    for i in range (len(sys.argv)):
+    for i in range(len(sys.argv)):
         if i != 0:
             my_list.append(sys.argv[i])
     save_to_json(my_list, filename)
