20ba7859e2d4fa358a75582e9fb898d134966143
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-07-26 16:23:08 +0100

    wait

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module returns the dictionary description with
+    simple data structure for json serialization of an object
+"""
+import json
+
+
+def class_to_json(obj):
+    """This function return the dictionart description with
+    simple data structure of json serializzation of an object
+    """
+    json.dumps(obj)
