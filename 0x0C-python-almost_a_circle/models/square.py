9f7fccead302ffbdb6e557b6bffb59c72aabbad1
Emmanuel142 <Emmanuel.ayoade.142@gmail.com>
2025-08-02 14:20:51 +0100

    Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module defines a class of Square.
+    It inherits from Rectangle module
+    and defines a square
+"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This class defines a square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """This init inherit from the Rectangle
+        Args:
+            size (int): size of the square
+            x (int): x coordinate
+            y (int): y coodinate
+            id (int): inherit id from Base
+        """
+        super().__init__(width, height, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """This attribute returns the size
+        of the Square
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """This set the size of the square
+        """
+        self.width = value
+        self.height = value
